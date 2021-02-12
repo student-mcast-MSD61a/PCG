b1f3145781f9abda94174e3ae78768816e582c79
student-mcast-MSD61a <56297786+student-mcast-MSD61a@users.noreply.github.com>
2021-02-12 14:35:38 +0100

    Task 3 - Added car tracks (3 Levels)

---
+++ Task 3/Scripts/PathMaker.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PathShape
+{
+    public Vector3[] shape = new Vector3[] { -Vector3.up, Vector3.up, -Vector3.up };
+
+}
+
+
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshFilter))]
+public class PathMaker : MonoBehaviour
+{
+
+    [SerializeField]
+    private int subMeshSize = 1;
+
+    [SerializeField]
+    private Transform[] path;
+
+    [SerializeField]
+    private PathShape pathShape;
+
+    // Update is called once per frame
+    void Update()
+    {
+        MeshFilter meshFilter = this.GetComponent<MeshFilter>();
+        MeshBuilder meshBuilder = new MeshBuilder(subMeshSize);
+
+        Vector3[] prevShape = TranslateShape(
+            path[path.Length-1].transform.position,
+            (path[0].transform.position - path[path.Length-1].transform.position).normalized,
+            pathShape
+            );
+
+        for (int i = 0; i < path.Length; i++)
+        {
+            Vector3[] currentShape = TranslateShape(
+            path[i].transform.position,
+            (path[(i+1) % path.Length].transform.position - path[i].transform.position).normalized,
+            pathShape
+            );
+
+            for(int j = 0; j < currentShape.Length - 1; j++)
+            {
+                meshBuilder.BuildTriangle(prevShape[j], currentShape[j], currentShape[j + 1] , 0);
+                meshBuilder.BuildTriangle(prevShape[j + 1], prevShape[j], currentShape[j+1], 0);
+            }
+
+            prevShape = currentShape;
+        }
+
+
+        meshFilter.mesh = meshBuilder.CreateMesh();
+    }
+
+    private Vector3[] TranslateShape(Vector3 point, Vector3 forward, PathShape shape)
+    {
+        Vector3[] translatedShape = new Vector3[shape.shape.Length];
+
+        Quaternion forwardRotation = Quaternion.LookRotation(forward);
+
+        for (int i = 0; i < shape.shape.Length; i ++)
+        {
+            translatedShape[i] = (forwardRotation * shape.shape[i]) + point;
+        }
+
+        return translatedShape;
+    }
+
+}
---
+++ Task 3/Scripts/RoadMaker.cs
@@ -0,0 +1,184 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshCollider))]
+public class RoadMaker : MonoBehaviour
+{
+    [SerializeField]
+    private float radius = 30f; // this defines the radius of the path
+
+    [SerializeField]
+    private float segments = 300f;
+
+    [SerializeField]
+    private float lineWidth = 0.3f; // middle white line road marker
+
+    [SerializeField]
+    private float roadWidth = 8f; // width of the road on each side of the line
+
+    [SerializeField]
+    private float edgeWidth = 1f; // width of our road barrier at the edge of our road
+
+    [SerializeField]
+    private float edgeHeight = 1f;
+
+    [SerializeField]
+    private int submeshSize = 6;
+
+    [SerializeField]
+    private float wavyness = 5f;
+
+    [SerializeField]
+    private float waveScale = 0.1f;
+
+    [SerializeField]
+    private Vector2 waveOffset;
+
+    [SerializeField]
+    private Vector2 waveStep = new Vector2(0.01f, 0.01f);
+
+    [SerializeField]
+    private bool stripeCheck = true;
+
+    [SerializeField]
+    private GameObject car;
+
+    void Start()
+    {
+        MeshFilter meshFilter = this.GetComponent<MeshFilter>();
+
+        MeshCollider meshCollider = this.GetComponent<MeshCollider>();
+
+        MeshBuilder meshBuilder = new MeshBuilder(submeshSize);
+
+
+        //1. Divide the circular race track into segments denoted in degrees and each point is defined by each segment
+        //   Create the points and store them in a list
+        float segmentDegrees = 360f / segments;
+
+        List<Vector3> points = new List<Vector3>();
+
+        for (float degrees = 0; degrees < 360f; degrees += segmentDegrees)
+        {
+            Vector3 point = Quaternion.AngleAxis(degrees, Vector3.up) * Vector3.forward * radius;
+            points.Add(point);
+        }
+
+        Vector2 wave = this.waveOffset;
+
+        for (int i = 0; i < points.Count; i++)
+        {
+            wave += waveStep;
+
+            Vector3 point = points[i];
+            Vector3 centreDirection = point.normalized;
+
+            float noise = Mathf.PerlinNoise(wave.x * waveScale, wave.y * waveScale);
+            noise *= wavyness;
+
+            float control = Mathf.PingPong(i, points.Count / 2f) / (points.Count / 2f);
+
+            points[i] += centreDirection * noise * control;
+        }
+
+        //2. function to define the path - the path is defined by each segment
+        for (int i = 1; i < points.Count + 1; i++)
+        {
+            Vector3 pPrev = points[i - 1];
+            Vector3 pCurr = points[i % points.Count];
+            Vector3 pNext = points[(i + 1) % points.Count];
+
+            ExtrudeRoad(meshBuilder, pPrev, pCurr, pNext);
+        }
+
+        car.transform.position = points[0];
+        car.transform.LookAt(points[1]);
+
+        meshFilter.mesh = meshBuilder.CreateMesh();
+
+        meshCollider.sharedMesh = meshFilter.mesh;
+    }
+
+    //3. This method will used to create the different segments for each segment we are going to draw the road marker 
+    //   (i.e. white line in the middle), draw the road on each side of the line, draw the edges - all these are going 
+    //   to be placed in different positions
+    private void ExtrudeRoad(MeshBuilder meshBuilder, Vector3 pPrev, Vector3 pCurr, Vector3 pNext)
+    {
+        //Road Line
+        Vector3 offset = Vector3.zero;
+        Vector3 targetOffset = Vector3.forward * lineWidth;
+
+        MakeRoadQuad(meshBuilder, pPrev, pCurr, pNext, offset, targetOffset, 0);
+
+        //Road
+        offset += targetOffset;
+        targetOffset = Vector3.forward * roadWidth;
+
+        MakeRoadQuad(meshBuilder, pPrev, pCurr, pNext, offset, targetOffset, 1);
+
+        int stripeSubmesh = 2;
+
+        if (stripeCheck)
+        {
+            stripeSubmesh = 3;
+        }
+
+        stripeCheck = !stripeCheck;
+
+        //Edge
+        offset += targetOffset;
+        targetOffset = Vector3.up * edgeHeight;
+
+        MakeRoadQuad(meshBuilder, pPrev, pCurr, pNext, offset, targetOffset, stripeSubmesh);
+
+        //Edge Top
+        offset += targetOffset;
+        targetOffset = Vector3.forward * edgeWidth;
+
+        MakeRoadQuad(meshBuilder, pPrev, pCurr, pNext, offset, targetOffset, stripeSubmesh);
+
+        //Edge
+        offset += targetOffset;
+        targetOffset = -Vector3.up * edgeHeight;
+
+        MakeRoadQuad(meshBuilder, pPrev, pCurr, pNext, offset, targetOffset, stripeSubmesh);
+
+    }
+
+    //4. Create each quad
+    private void MakeRoadQuad(MeshBuilder meshBuilder, Vector3 pPrev, Vector3 pCurr, Vector3 pNext,
+                              Vector3 offset, Vector3 targetOffset, int submesh)
+    {
+        Vector3 forward = (pNext - pCurr).normalized;
+        Vector3 forwardPrev = (pCurr - pPrev).normalized;
+
+        //Build Outer Track
+        Quaternion perp = Quaternion.LookRotation(Vector3.Cross(forward, Vector3.up));
+        Quaternion perpPrev = Quaternion.LookRotation(Vector3.Cross(forwardPrev, Vector3.up));
+
+        Vector3 topLeft = pCurr + (perpPrev * offset);
+        Vector3 topRight = pCurr + (perpPrev * (offset + targetOffset));
+
+        Vector3 bottomLeft = pNext + (perp * offset);
+        Vector3 bottomRight = pNext + (perp * (offset + targetOffset));
+
+        meshBuilder.BuildTriangle(topLeft, topRight, bottomLeft, submesh);
+        meshBuilder.BuildTriangle(topRight,bottomRight,bottomLeft, submesh);
+
+        //Build Inner Track
+        perp = Quaternion.LookRotation(Vector3.Cross(-forward, Vector3.up));
+        perpPrev = Quaternion.LookRotation(Vector3.Cross(-forwardPrev, Vector3.up));
+
+        topLeft = pCurr + (perpPrev * offset);
+        topRight = pCurr + (perpPrev * (offset + targetOffset));
+
+        bottomLeft = pNext + (perp * offset);
+        bottomRight = pNext + (perp * (offset + targetOffset));
+
+        meshBuilder.BuildTriangle(bottomLeft, bottomRight, topLeft, submesh);
+        meshBuilder.BuildTriangle(bottomRight, topRight, topLeft, submesh);
+    }
+}
